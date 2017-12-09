b4691051a48b70e3da56c5c1e3cc7d4db81fc204
Miro Svrtan <miro@mirosvrtan.me>
2017-12-09 13:56:44 +0100

    Switch to ProductRepository

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\FakeProductRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,14 +15,16 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProductsController extends Controller
 {
-    /**
-     * @var FakeProductRepository
-     */
+    /** @var ProductRepository */
     private $repository;
+    /** @var EntityManager */
+    private $entityManager;
 
-    public function __construct(FakeProductRepository $repository)
+    public function __construct(ProductRepository $repository,
+        EntityManagerInterface $entityManager)
     {
         $this->repository = $repository;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -28,36 +32,40 @@ class ProductsController extends Controller
      */
     public function listAction()
     {
-        $data = [
-            'products' => $this->repository->findAll(),
-        ];
+        $product = new Product();
+        $product->setTitle('zzz'.time());
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        $data = [ 'products' => $this->repository->findAll(),];
 
         return $this->render('products.html.twig', $data);
     }
 
-    /**
-     * @Route("/products/{page}")
-     */
+    /** @Route("/products/{page}") */
     public function pagingAction($page)
     {
-
-        $data = [
-            'product' => $this->repository->findBy([], [], 1)[0],
-        ];
+        $data = [ 'product' => $this->repository->findBy([], [], 1)[0],];
 
         return $this->render('product.html.twig', $data);
     }
 
-    /**
-     * @Route("/product/{id}")
-     */
+    /** @Route("/product/{id}") */
     public function showAction($id)
     {
-        $data = [
-            'product' => $this->repository->find($id),
-        ];
+        $data = [ 'product' => $this->repository->find($id),];
 
         return $this->render('product.html.twig', $data);
     }
 
 }
+
+/**
+ * @ORM\OneToMany(targetEntity="App\Entity\Product", mappedBy="category")
+ */
+
+/**
+ * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="products")
+ * @ORM\JoinColumn(nullable=true)
+ */
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -12,17 +12,4 @@ class ProductRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Product::class);
     }
-
-    /*
-    public function findBySomething($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->where('p.something = :value')->setParameter('value', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 }
