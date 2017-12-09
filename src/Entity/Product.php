627d507bc6e0d3183ca31338d34fe8a5ffa9441f
Miro Svrtan <miro@mirosvrtan.me>
2017-12-09 12:51:21 +0100

    Add product entity & repository built by maker

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -16,5 +16,29 @@ class Product
      */
     private $id;
 
-    // add your own fields
+    /**
+     * @ORM\Column(type="string",length=200)
+     */
+    private $title;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
 }
